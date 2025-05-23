8536c3dca40fd41d61c8db3fc4246db38f7e5399
taewaem <monkey587@naver.com>
2025-05-23 12:31:16 +0900

    feat: UtilPanel 추가

--- src/store/frame/UtilPanel.java
+++ src/store/frame/UtilPanel.java
@@ -5,22 +5,10 @@ import javax.swing.*;
 
 public class UtilPanel{
 
-    public void goToPage(JPanel newPage) {
-
-        MainFrame.pageStack.push((JPanel) MainFrame.mainFrame.getContentPane()); // 현재 패널 저장
-        MainFrame.mainFrame.setContentPane(newPage); // 새로운 패널로 전환
-        MainFrame.mainFrame.revalidate();
-        MainFrame.mainFrame.repaint();
+    public void switchPanel(JPanel container, JPanel newPanel) {
+        container.removeAll();             // 기존 컴포넌트 제거
+        container.add(newPanel);          // 새 패널 추가
+        container.revalidate();           // 레이아웃 새로 계산
+        container.repaint();              // 다시 그리기
     }
-
-    public void goBackPage() {
-        if (!MainFrame.pageStack.isEmpty()) {
-            JPanel backPanel = MainFrame.pageStack.pop();
-            MainFrame.mainFrame.setContentPane(backPanel); // 이전 패널로 전환
-            MainFrame.mainFrame.revalidate();
-            MainFrame.mainFrame.repaint();
-
-        }
-    }
-
 }
