543e180378acabe458f2b0f80446e69a57167f7a
taewaem <monkey587@naver.com>
2025-05-23 12:30:21 +0900

    feat: ProductPanel 추가

--- src/store/frame/ProductPanel.java
+++ src/store/frame/ProductPanel.java
@@ -1,25 +1,30 @@
 package store.frame;
 
 import store.cart.Cart;
-import store.frame.CartPanel;
-import store.frame.UtilPanel;
 import store.product.Product;
 import store.product.ProductDescription;
 
 import javax.swing.*;
 import java.awt.*;
 import java.net.URL;
+import java.text.DecimalFormat;
 
 public class ProductPanel extends JPanel {
 
-    private UtilPanel utilPanel = new UtilPanel();
     private JButton cartButton;
-    private JButton backButton;
+    private JButton storeButton;
+    private JButton checkButton;
     private Cart cart;
+    private BackgroundPanel backgroundPanel;
+    private DecimalFormat df = new DecimalFormat("###,###");
+    private UtilPanel utilPanel = new UtilPanel();
 
 
     public ProductPanel(Product product) {
         cart = new Cart();
+
+        backgroundPanel = new BackgroundPanel();
+
 
         // 전체 패널 배경: 연한 아이보리
         setBackground(new Color(250, 250, 245));
@@ -42,6 +47,7 @@ public class ProductPanel extends JPanel {
         ));
         imagePanel.add(imageLabel, BorderLayout.CENTER);
         imagePanel.setPreferredSize(new Dimension(300, 320));
+        imagePanel.setOpaque(false);
         add(imagePanel, BorderLayout.WEST);
 
         // 오른쪽 정보 패널
@@ -49,6 +55,7 @@ public class ProductPanel extends JPanel {
         infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
         infoPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 30));
         infoPanel.setBackground(new Color(250, 250, 245)); // 전체와 맞춤
+        infoPanel.setOpaque(false);
 
         // 제품명
         JLabel nameLabel = new JLabel(product.getName());
@@ -60,7 +67,7 @@ public class ProductPanel extends JPanel {
         infoPanel.add(Box.createVerticalStrut(25));
 
         // 제품 가격
-        JLabel priceLabel = new JLabel(MainFrame.df.format(product.getPrice()) + " 원");
+        JLabel priceLabel = new JLabel(df.format(product.getPrice()) + " 원");
         priceLabel.setFont(new Font("맑은 고딕", Font.BOLD, 22));
         priceLabel.setForeground(new Color(98, 0, 220)); // 조금 더 부드러운 보라톤
         priceLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
@@ -94,6 +101,7 @@ public class ProductPanel extends JPanel {
         JLabel quantityLabel = new JLabel("수량:");
         quantityLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
         quantityLabel.setForeground(new Color(60, 60, 60));
+        quantityPanel.setOpaque(false);
 
         SpinnerNumberModel spinnerModel = new SpinnerNumberModel(1, 1, 99, 1);
         JSpinner quantitySpinner = new JSpinner(spinnerModel);
@@ -114,14 +122,38 @@ public class ProductPanel extends JPanel {
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 15, 0));
         buttonPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
         buttonPanel.setBackground(new Color(250, 250, 245));
+        buttonPanel.setOpaque(false);
+
+        checkButton = new JButton("상태");
+        checkButton.setBackground(new Color(128, 128, 128));
+        checkButton.setForeground(Color.WHITE);
+        checkButton.setFocusPainted(false);
+        checkButton.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+        checkButton.setPreferredSize(new Dimension(100, 40));
+
+        checkButton.addActionListener(e -> {
+//            utilPanel.switchPanel(this, new CheckPanel());
+        });
+
+
+        storeButton = new JButton("상점");
+        storeButton.setBackground(new Color(128, 128, 128));
+        storeButton.setForeground(Color.WHITE);
+        storeButton.setFocusPainted(false);
+        storeButton.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+        storeButton.setPreferredSize(new Dimension(100, 40));
+
+        storeButton.addActionListener(e -> {
+            utilPanel.switchPanel(this, new StorePanel());
+        });
 
         cartButton = new JButton("장바구니 담기");
         cartButton.setBackground(new Color(98, 0, 220));
         cartButton.setForeground(Color.WHITE);
         cartButton.setFocusPainted(false);
-        cartButton.setFont(new Font("맑은 고딕", Font.BOLD, 16));
-        cartButton.setPreferredSize(new Dimension(140, 45));
-        cartButton.setBorder(BorderFactory.createEmptyBorder(8, 15, 8, 15));
+        cartButton.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+        cartButton.setPreferredSize(new Dimension(140, 40));
+        cartButton.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
         cartButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
         // 호버 효과 (간단히)
@@ -134,30 +166,14 @@ public class ProductPanel extends JPanel {
             }
         });
 
-        backButton = new JButton("이전");
-        backButton.setBackground(new Color(150, 150, 150));
-        backButton.setForeground(Color.WHITE);
-        backButton.setFocusPainted(false);
-        backButton.setFont(new Font("맑은 고딕", Font.BOLD, 16));
-        backButton.setPreferredSize(new Dimension(90, 45));
-        backButton.setBorder(BorderFactory.createEmptyBorder(8, 15, 8, 15));
-        backButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-
-        backButton.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                backButton.setBackground(new Color(180, 180, 180));
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                backButton.setBackground(new Color(150, 150, 150));
-            }
-        });
-
+        buttonPanel.add(checkButton);
+        buttonPanel.add(storeButton);
         buttonPanel.add(cartButton);
-        buttonPanel.add(backButton);
 
         infoPanel.add(buttonPanel);
 
         add(infoPanel, BorderLayout.CENTER);
+
 
         // 버튼 기능
         cartButton.addActionListener(e -> {
@@ -173,14 +189,11 @@ public class ProductPanel extends JPanel {
 
             if (result == JOptionPane.YES_OPTION) {
                 System.out.println("장바구니 페이지로 이동");
-                utilPanel.goToPage(new CartPanel(cart));
+                utilPanel.switchPanel(this, new CartPanel(cart));
+
             }
         });
 
-        backButton.addActionListener(e -> {
-            utilPanel.goBackPage();
-            System.out.println("이전 페이지로 이동");
-        });
 
         setPreferredSize(new Dimension(800, 420));
     }
@@ -201,5 +214,12 @@ public class ProductPanel extends JPanel {
         label.setForeground(new Color(70, 70, 70));
         label.setAlignmentX(Component.LEFT_ALIGNMENT);
         return label;
+    }
+
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        backgroundPanel.paintBackground(g, this);
     }
 }
